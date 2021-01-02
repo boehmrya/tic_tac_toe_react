d0ec1aa119e7d21c295410a78854e59e965fd2b9
Ryan <ryan.w.boehm@gmail.com>
2021-01-01 21:02:54 -0600

    adjusted board to handle game state

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button 
+      <button
         className="square"
         onClick={() => this.setState({value: 'X'}) }
       >
@@ -24,8 +24,15 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
